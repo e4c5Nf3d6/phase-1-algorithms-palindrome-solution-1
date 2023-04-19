da79f42abc40ec8d3afd3ea4202e4031f1bbbc70
Maria Jenkins <macjenkin@gmail.com>
2023-04-19 15:45:34 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  return word === word.split('').reverse().join('')
 }
 
 /* 
-  Add your pseudocode here
+reverse the input string
+
+return truthiness of: the reversed string is the same as the input
 */
 
 /*
-  Add written explanation of your solution here
+Reverses the string and checks if it is the same as the original string.
+Returns true if it is the same, false if it is not.
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +23,21 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome(""));
+  
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("i"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("noon"));
 }
 
 module.exports = isPalindrome;
